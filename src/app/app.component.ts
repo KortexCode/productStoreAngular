9a93fe8d75eeff8a1ab2be2ab1bad4e768536e6c
KortexDev <cacortesl01@gmail.com>
2025-02-12 16:43:23 -0500

    Se crean rutas para el router, se agregan estilos a la tabla de productos y navegación de botones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { ListProductsComponent } from "./components/list-products/list-products.component";
 
 @Component({
   selector: 'app-root',
--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { Product } from '../../interface/product';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list-products',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.scss'
 })
 export class ListProductsComponent {
-
+  listProducts: Product[] = [
+    {
+      id:1,
+      name: 'Warcraft 3 reforged',
+      description: 'Juego de estrategia en tiempo real',
+      price: 250,
+      stock: 20,
+    },
+    {
+      id:2,
+      name: 'World of warcraft Classic',
+      description: 'Juego de rol en tiempo real',
+      price: 500,
+      stock: 10,
+    }
+  ]
 }
