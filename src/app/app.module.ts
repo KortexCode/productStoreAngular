f4101c43f7b959cf05b31fcbebfeb76fb2750f68
KortexDev <cacortesl01@gmail.com>
2025-02-12 23:44:34 -0500

    Estilos al componente add-edit-componente, Configurando validaciones en el form

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+
+@NgModule({
+  imports: [
+    ReactiveFormsModule, // <-- Asegúrate de agregar esto
+  ],
+})
+export class AppModule {}
+
--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit-product',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './add-edit-product.component.html',
-  styleUrl: './add-edit-product.component.scss'
+  styleUrl: './add-edit-product.component.scss',
 })
 export class AddEditProductComponent {
-
+  formAddControl: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.formAddControl = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', Validators.required],
+      stock: ['', Validators.required]
+    });
+  }
 }
