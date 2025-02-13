e1909f80ba39ade57f7c8f216972e2b02e772eee
KortexDev <cacortesl01@gmail.com>
2025-02-13 08:49:41 -0500

    Se termina de configurar validaciones, se obtienen datos del formulario

--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Product } from '../../interface/product';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-add-edit-product',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, NgIf],
   templateUrl: './add-edit-product.component.html',
   styleUrl: './add-edit-product.component.scss',
 })
@@ -17,5 +19,17 @@ export class AddEditProductComponent {
       price: ['', Validators.required],
       stock: ['', Validators.required]
     });
+  }
+  addProduct() {
+    console.log(this.formAddControl.value);
+    const {name, description, price, stock} = this.formAddControl.value;
+    const product: Product = {
+      name: name,
+      description: description,
+      price: price,
+      stock: stock
+    }
+
+    console.log("El producto:", product)
   }
 }
