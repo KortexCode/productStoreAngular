dc8b5d650e8c62cfbb9db737ee71b3032b5f05ac
KortexDev <cacortesl01@gmail.com>
2025-03-18 12:02:43 -0500

    Se termina de maquetar componente login, cambios en rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { ListProductsComponent } from './components/list-products/list-products.component';
 import { AddEditProductComponent } from './components/add-edit-product/add-edit-product.component';
+import {LoginComponent} from './components/login/login.component';
 
 export const routes: Routes = [
-  {path: '', component: ListProductsComponent},//RUTA PRINCIPAL
-  {path: 'add', component: AddEditProductComponent},
-  {path: 'edit/:id', component: AddEditProductComponent},
-  { path: '**',   redirectTo: '', pathMatch: 'full' },//RUTA COMODÍN
+  {path: '', redirectTo:"login", pathMatch:"full"},//RUTA PRINCIPAL
+  {path: 'login', component: LoginComponent},//Ruta de lista de productos
+  {path: 'products', component: ListProductsComponent},//Ruta de lista de productos
+  {path: 'products/add', component: AddEditProductComponent},
+  {path: 'products/edit/:id', component: AddEditProductComponent},
+  { path: '**',   redirectTo: 'login', pathMatch: 'full' },//RUTA COMODÍN
 ];
